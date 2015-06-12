ff4ac74f606c2c7491a1c3897441b0439072a4b1
Jacy Wang <jacywj@gmail.com>
2015-06-12 11:17:36 -0700

    Initialize repo

---
+++ Receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts
+//
+//  Created by JIAN WANG on 6/12/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
